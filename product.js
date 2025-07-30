1f457670e6d70bee6391cd95940ad6a7a2dba5d1
Python369-gn <elhadjoumarhadiatou1@gmail.com>
2025-07-30 19:06:12 +0200

    Update product.js
    j'ai changé les images

--- product.js
+++ product.js
@@ -2,12 +2,12 @@
 
         // Données des produits (stockés dans localStorage)  
         let products = JSON.parse(localStorage.getItem('products')) || [
-            { id: 1, name: "Smartphone Écologique", price: "30 000 000", category: "electronics", image: "image/phone.jpg"},
-            { id: 2, name: "Casque Audio Verte", price: "15 000 000", category: "electronics", image: "image/casque.jpg" },
-            { id: 3, name: "Montre Connectée", price: "10 000 000", category: "electronics", image: "image/montre.jpg" },
-            { id: 4, name: "T-Shirt Bio", price: "25 000 000", category: "clothing", image: "image/T-Sh.jpg" },
-            { id: 5, name: "Jeans Durable", price: "19 000 000", category: "clothing", image: "image/jean.jpg" },
-            { id: 6, name: "Lampe LED", price: "5 000 000", category: "home", image: "image/lampe.jpg" },
-            { id: 7, name: "Coussin Naturel", price: "14 000 000", category: "home", image: "image/coussin.jpg" },
-            { id: 8, name: "Enceinte Solaire", price: "5 000 000", category: "electronics", image: "image/anceinte.jpg" }
+            { id: 1, name: "Smartphone Écologique", price: "30 000 000", category: "electronics", image: "phone.jpg"},
+            { id: 2, name: "Casque Audio Verte", price: "15 000 000", category: "electronics", image: "casque.jpg" },
+            { id: 3, name: "Montre Connectée", price: "10 000 000", category: "electronics", image: "montre.jpg" },
+            { id: 4, name: "T-Shirt Bio", price: "25 000 000", category: "clothing", image: "T-Sh.jpg" },
+            { id: 5, name: "Jeans Durable", price: "19 000 000", category: "clothing", image: "jean.jpg" },
+            { id: 6, name: "Lampe LED", price: "5 000 000", category: "home", image: "lampe.jpg" },
+            { id: 7, name: "Coussin Naturel", price: "14 000 000", category: "home", image: "coussin.jpg" },
+            { id: 8, name: "Enceinte Solaire", price: "5 000 000", category: "electronics", image: "anceinte.jpg" }
         ];
